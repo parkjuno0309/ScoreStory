0bca9ae293608e90715d5d6031c6084019ded4ac
laurappark <119468501+laurappark@users.noreply.github.com>
2023-04-03 19:11:05 -0500

    Update post css

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -7,13 +7,13 @@ const Post = ({postInfo}) => {
     return (
       // replace with a Card from bootstrap instead
       <div className="card mb-3">
-        <h5 className="card-header bg-light "> {postInfo.item}</h5>
         <div className="card-body">
-          <h5 className="card-title">{postInfo.name}</h5>
-          <p class="card-text">{postInfo.email}</p>
+        <h6 className="card-text"> {postInfo.name} Needs:</h6>
+          <h5 className="card-title">{postInfo.item}</h5>
           <p class="card-text">
-            {postInfo.date}, {postInfo.startTime}-{postInfo.endTime}
+            On {postInfo.date} From {postInfo.startTime}-{postInfo.endTime}
           </p>
+          <p class="card-text">{postInfo.email}</p>
         </div>
         {/* <div className="post">
           <div className="name-email">
