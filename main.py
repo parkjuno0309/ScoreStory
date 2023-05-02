fd46d204e4a74017aff4863540b3d5c0a634661a
JACK WHEELER <jackwheeler@JACKs-MacBook-Air.local>
2023-05-01 20:32:49 -0500

    building out game class

--- main.py
+++ main.py
@@ -2,79 +2,123 @@ import requests
 from pprint import PrettyPrinter
 import json
 
-BASE_URL = "https://api.sportsdata.io"
-API_KEY = "5dfb1139e28a480d9e3f84f84f340c2b"
-printer = PrettyPrinter()
-
-date = input("Enter date of MLB game:") # format should be "2017-JUL-31"
-team = input("Enter the team that played on that date:") # format should be "NYY"
-
-#####
-
-# we should clean the data above using chatGPT here -> return date, team
-
-#####
-
-# Get Game ID
-request_url = f"{BASE_URL}/v3/mlb/stats/json/BoxScores/{date}?key={API_KEY}"
-response = requests.get(request_url)
-
-if response.status_code == 200:
-    print("successful response")
-    games_data = response.json()
-
-print(len(games_data))
-
-game_id = -1
-
-for i in range(len(games_data)):
-    if games_data[i]['Game']['HomeTeam'] == team or games_data[i]['Game']['AwayTeam'] == team:
-        desired_game_index = i
-        game_id = games_data[i]['Game']['GameID']
-        break
-
-if game_id == -1:
-    print("Game not found in database")
-else:
-    print(game_id)
-
-#####
-request_url3 = f"{BASE_URL}/v3/mlb/scores/json/AllTeams?key={API_KEY}"
-# Get play-by-play data for specific game using game_id
-request_url2 = f"{BASE_URL}/v3/mlb/pbp/json/PlayByPlay/{game_id}?key={API_KEY}"
-response2 = requests.get(request_url3)
-
-if response2.status_code == 200:
-    print("successful response")
-    pbp_data = response2.json()
-
-printer.pprint(pbp_data)
-
-
-
-
 
 class Game:
     # constructor
     def __init__(self, game_date, team_abbreviation):
-        self.game_id = game_id
+        self.home_pitchers = None
+        self.away_pitchers = None
+        self.teams = None
+        self.game = None
+        self.pbp = None
+        self.game_date = game_date
+        self.team_abbreviation = team_abbreviation
         self.standings = None
         self.starting_pitchers = None
         self.scoring_plays = None
         self.pitching_changes = None
         self.Result = None
+        self.BASE_URL = "https://api.sportsdata.io"
+        self.API_KEY = "5dfb1139e28a480d9e3f84f84f340c2b"
 
     def get_team_name(self,abbreviation):
-        request_url3 = f"{BASE_URL}/v3/mlb/scores/json/AllTeams?key={API_KEY}"
-        if response2.status_code == 200:
-            team_data = response2.json()
+        request_url = f"{self.BASE_URL}/v3/mlb/scores/json/AllTeams?key={self.API_KEY}"
+        response = requests.get(request_url)
+        if response.status_code == 200:
+            team_data = response.json()
             for team in team_data:
                 if(team['Active'] == True):
                     if(team['Key'] == abbreviation):
-                        return (team['City'] + team['Name'])
+                        return (team['City'] + ' ' + team['Name'])
                     
-my_instance = Game('2012-OCT-24', 'DET')
-print(my_instance.get_team_name('DET'))
+    def get_player_name(self, id):
+        request_url = f"{self.BASE_URL}/v3/mlb/scores/json/Player/{id}?key={self.API_KEY}"
+        response = requests.get(request_url)
+        if response.status_code == 200:
+            data = response.json()
+            name = f"{data['FirstName']} {data['LastName']}"
+        return name
+    
+                    
+    def get_pbp(self):
+        request_url = f"{self.BASE_URL}/v3/mlb/stats/json/BoxScores/{self.game_date}?key={self.API_KEY}"
+        response = requests.get(request_url)
+        if response.status_code == 200:
+            print("successful response")
+            games_data = response.json()
+
+        game_id = -1
+
+        for i in range(len(games_data)):
+            if games_data[i]['Game']['HomeTeam'] == self.team_abbreviation or games_data[i]['Game']['AwayTeam'] == self.team_abbreviation:
+                desired_game_index = i
+                game_id = games_data[i]['Game']['GameID']
+                break
+
+        if game_id == -1:
+            print("Game not found in database")
+        
+        
+        # Get play-by-play data for specific game using game_id
+        request_url2 = f"{self.BASE_URL}/v3/mlb/pbp/json/PlayByPlay/{game_id}?key={self.API_KEY}"
+        response2 = requests.get(request_url2)
+
+        if response2.status_code == 200:
+            #print("successful response")
+            pbp_data = response2.json()
+            self.pbp = pbp_data['Plays']
+            self.game = pbp_data['Game']
+                    
+    def home_away(self):
+        home = self.game['HomeTeam']
+        away = self.game['AwayTeam']
+        self.teams = {'Home': self.get_team_name(home),'Away': self.get_team_name(away)}
+
+    def get_result(self):
+        homeTotalRuns = 0
+        homeInningsScored = []
+        awayTotalRuns = 0
+        a
+        innings_data = self.game['Innings']
+        for inning in innings_data:
+            homeTotalRuns += inning['HomeTeamRuns']
+            awayTotalRuns += inning['awayTeamRuns']
+        self.Result = {'Home': homeTotalRuns, 'Away': awayTotalRuns}
+
+
+    def get_pitching_data(self):
+        homeid = self.game['HomeTeamStartingPitcherID']
+        awayid = self.game['AwayTeamStartingPitcherID']
+        homestarter = self.get_player_name(homeid)
+        awaystarter = self.get_player_name(awayid)
+        home_ids = {}
+        away_ids = {}
+        home_ids[homeid] = 0
+        away_ids[awayid] = 0
+        for play in self.pbp:
+            if(play['InningHalf'] == 'T'):
+                if(play['PitcherID'] != awayid):
+                    away_ids[play['PitcherID']] == 0
+                    awayid = play['PitcherID']
+                else:
+                    away_ids[play['PitcherID']] += 1
+            else:
+                if(play['PitcherID'] != homeid):
+                    home_ids[play['PitcherID']] == 0
+                    homeid = play['PitcherID']
+                else:
+                    home_ids[play['PitcherID']] += 1
+                    
+        
+        self.home_pitchers = home_ids
+        self.away_pitchers = away_ids
+
+                    
+my_instance = Game('2023-APR-30', 'LAD')
+my_instance.get_pbp()
+my_instance.home_away()
+my_instance.get_pitching_data()
+print(my_instance.home_pitchers)
 
 
 
